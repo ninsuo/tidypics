74e25837dceaf55be49745585dbb04260c5cb9b2
alain tiemblo <ninsuo@gmail.com>
2016-04-16 13:48:11 +0200

    first version

---
+++ README.md
@@ -0,0 +1,111 @@
+# TidyPics
+
+A small script that automatically:
+- reorder pictures coming from a camera
+- create gifs from speedshots
+
+## Example:
+
+```sh
+alain@home.fuz.org ~/Desktop/test
+$ tree .
+.
+├── P1060988.JPG
+├── P1060989.JPG
+├── P1060990.JPG
+├── P1060991.JPG
+├── P1060992.JPG
+├── P1060993.JPG
+├── P1060994.JPG
+├── P1060995.JPG
+├── P1060996.JPG
+├── P1060997.JPG
+├── P1060998.JPG
+├── P1060999.JPG
+├── P1070001.JPG
+├── P1070002.JPG
+├── P1070003.JPG
+├── P1070004.JPG
+├── P1070005.JPG
+├── P1070006.JPG
+├── P1070009.JPG
+├── P1070010.JPG
+├── P1070011.JPG
+├── P1070012.JPG
+├── P1070013.JPG
+├── P1070014.JPG
+├── P1070015.JPG
+├── P1070016.JPG
+├── P1070017.JPG
+├── P1070018.JPG
+...
+alain@home.fuz.org ~/Desktop/test
+$ tidypics
+$ tree .
+.
+└── 2016-04-12
+    ├── 20-34-26.jpg
+    ├── 20-34-28.jpg
+    ├── 20-34-34.jpg
+    ├── 20-34-36.jpg
+    ├── 20-34-54.jpg
+    ├── 20-34-56.jpg
+    ├── 20-35-04.jpg
+    ├── 20-35-08_1.jpg
+    ├── 20-35-08_2.jpg
+    ├── 20-35-24.jpg
+    ├── 20-35-36_1.jpg
+    ├── 20-35-36_2.jpg
+    ├── 20-36-06.jpg
+    ├── 20-36-14.jpg
+    ├── 20-36-34.jpg
+    ├── 20-36-46_1.jpg
+    ├── 20-36-46_2.jpg
+    ├── 20-36-46_3.jpg
+    ├── 20-36-50.jpg
+    ├── 20-37-04.jpg
+    ├── 20-37-46.jpg
+    ├── 20-38-34
+    │   ├── 1.jpg
+    │   ├── 2.jpg
+    │   ├── 3.jpg
+    │   ├── 4.jpg
+    │   ├── 5.jpg
+    │   ├── 6.jpg
+    │   ├── 7.jpg
+    │   └── animation.gif
+    ├── 20-38-40
+    │   ├── 1.jpg
+    │   ├── 2.jpg
+    │   ├── 3.jpg
+    ...
+```
+
+## Installation:
+
+1) download:
+
+```
+cd /Users/alain/scripts
+git clone git@github.com:ninsuo/tidypics.git
+```
+
+2) create an alias in your shell config:
+
+```sh
+alias tidypics='php /Users/alain/scripts/tidypics/tidypics.php'
+```
+
+## Usage
+
+In a directory containing pictures, type `tidypics`.
+
+## Requirements:
+
+- php >= 5.5
+- ImageMagick (for creating gifs)
+
+## About
+
+My camera natively create gifs but I f*cked up last time and I almost only did speedshots.
+Needed to create some workaround.
---
+++ tidypics.php
@@ -0,0 +1,132 @@
+<?php
+
+/*
+ * This script should be run inside a directory containing a bunch of non-sorted and badly named pics.
+ *
+ * It will:
+ * - Create 1 directory per day of pictures (example: 2016-04-12) and move all pictures taken that day inside
+ * - Rename all pictures using the time they were taken (example: 21-33-28.jpg) managing speedshots as well
+ * - Create gifs with your speedshots (group of >= 4 shots taken in less than 1 sec)
+ *
+ * Requirements:
+ * - Pictures should have their modification date corresponding to the date where they have been taken.
+ * - ImageMagick should be installed
+ */
+
+date_default_timezone_set('Europe/Paris');
+
+$path = $_SERVER['PWD'];
+
+$glob = glob("/{$path}/*");
+sort($glob, SORT_NATURAL);
+
+$array = [];
+foreach ($glob as $file)
+{
+    $mtime = filemtime($file);
+    $date = date("Y-m-d H:i:s", $mtime);
+    if (array_key_exists($date, $array) == false)
+    {
+        $array[$date] = 0;
+    }
+    $array[$date]++;
+}
+
+$list = [];
+foreach ($glob as $file)
+{
+    if (!is_file($file))
+    {
+        continue;
+    }
+
+    $mtime = filemtime($file);
+    if (!array_key_exists($mtime, $list)) {
+        $list[$mtime] = [];
+    }
+
+    $directory = date("Y-m-d", $mtime);
+    if (!is_dir($directory))
+    {
+        mkdir($directory);
+    }
+
+    $date = date("Y-m-d H:i:s", $mtime);
+    $ext  = strtolower(pathinfo($file, PATHINFO_EXTENSION));
+
+    if ($array[$date] == 1)
+    {
+        $newName = date("H-i-s", $mtime) . "." . $ext;
+        $newPath = "{$directory}/{$newName}";
+        echo "New name: {$newPath}\n";
+        rename($file, $newPath);
+        $list[$mtime][] = $newPath;
+    }
+    else
+    {
+        for ($count = 1; ($count <= $array[$date]); $count++)
+        {
+            $newName = date("H-i-s", $mtime) . "_{$count}." . $ext;
+            $newPath = "{$directory}/{$newName}";
+            if (!is_file($newPath))
+            {
+                echo "New name: {$newPath}\n";
+                rename($file, $newPath);
+                $list[$mtime][] = $newPath;
+                break ;
+            }
+        }
+    }
+}
+
+ksort($list);
+
+$groups = [];
+$group  = null;
+$old    = null;
+foreach ($list as $mtime => $batch)
+{
+    if (is_null($old))
+    {
+        $group = $batch;
+        $old   = $mtime;
+        continue ;
+    }
+
+    if ($mtime - $old <= 2)
+    {
+        $group = array_merge($group, $batch);
+        $old   = $mtime;
+        continue ;
+    }
+
+    $groups[$mtime] = $group;
+    $group          = $batch;
+    $old            = $mtime;
+}
+
+if (!is_null($group)) {
+    $groups[$old] = $group;
+}
+
+foreach ($groups as $mtime => $group) {
+    if (($c = count($group)) >= 5) {
+        $directory = date("Y-m-d/H-i-s", $mtime);
+        if (!is_dir($directory)) {
+            mkdir($directory, 0755, true);
+        }
+
+        $counter = 1;
+        foreach ($group as $file) {
+            $newName = str_repeat('0', strlen($c) - strlen($counter)) . $counter++;
+            $newPath = "{$directory}/{$newName}.jpg";
+            echo "New name: {$newPath}\n";
+            rename($file, $newPath);
+        }
+
+        $gifSource = "{$directory}/*.jpg";
+        $gifPath   = "{$directory}/animation.gif";
+        echo "Creating {$gifPath}...\n";
+        exec("convert -resize 768x576 -delay 25 -loop 0 {$gifSource} {$gifPath}");
+    }
+}
